a95518a16123b1becbc96b32e2e3c8bb3b91b411
jacobdlewis <jacobdlewis@gmail.com>
2015-04-13 16:35:53 -0500

    all tests but deep equal comparison passing

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,27 @@
 class LinkedListItem
   include Comparable
+  attr_reader :payload, :next_item, :last
+
+  def initialize(payload)
+    @payload = payload
+  end
+
+  def next_item=(next_item)
+      @next_item = next_item
+      raise ArgumentError if next_item == self
+  end
+
+  def last?
+    if next_item == nil
+      @last = true
+    else
+      @last = false
+    end
+  end
+
+  def <=> other
+    self.payload.to_s <=> other.payload.to_s
+  end
+
 end
+
