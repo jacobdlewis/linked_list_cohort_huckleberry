c5b28127153f5dc4d097118fc3835f3621d0e75c
jacobdlewis <jacobdlewis@gmail.com>
2015-04-14 18:37:53 -0500

    pushing multiple items works correctly

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,15 +8,20 @@ class LinkedList
   def push(ll_item)
     if @first_item.nil?
       @first_item = LinkedListItem.new(ll_item)
-    else
+    elsif @first_item.nil? == false && @first_item.next_item.nil?
       @first_item.next_item = LinkedListItem.new(ll_item)
+    else
+      current = @first_item
+      while current.next_item.nil? == false
+        current = current.next_item
+        current
+      end
+      current.next_item = LinkedListItem.new(ll_item)
     end
-    # elsif @first_item.next_item != true
-    #   @first_item.next_item(LinkedListItem.new(ll_item))
-    # end
   end
 
   def get(index)
+    raise IndexError, "Index can't be negative" if index < 0
     starting_point = @first_item
     index.times do
       next_item = starting_point.next_item
